a6664be2e41aed692033ff56943969fa83eb8aa5
Yi Feng <n.yifeng@digipen.edu>
2020-12-11 15:07:39 +0800

    transaction to pull data using Andy's axios and to update on positive scenerio

--- Frontend/src/Components/Transaction.js
+++ Frontend/src/Components/Transaction.js
@@ -5,25 +5,59 @@ import axios from 'axios';
 
 export default function Transaction (){
     let data = {custID:22}
-
+    let bal =0;
     const haha = useAxiosPost('https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/accounts/view', data)
-    console.log(haha);
-    let bal =  haha.data;
-    console.log(bal);
+    if(haha.loading){
+
+        console.log(haha.data[1]);
+        bal = haha.data[1].availableBal;
+    }
     function handleTransaction (event){
-        var number = Number(event.target[1].value.replace(/[^0-9.-]+/g,"")); // to save
+        var number = Number(event.target[2].value.replace(/[^0-9.-]+/g,"")); // to save
         console.log(number);
         if(number < bal){
-            let final = bal - number;
-            let data= {custID:22, amount: final};
-            
+            let updated = bal -number;
+            let newData = {custID:22, amount:updated};
+            console.log(updated);
+            const config = {
+                headers: {
+                  'Content-Type': 'application/json;charset=UTF-8',
+                  'Access-Control-Allow-Origin': '*',
+                  'x-api-key': 'IrZsuQz9Ns1h2yaHZwhmC2hjGq3xJXpG2KyqIJjG'
+                }
+              }
+            //let response = useAxiosPost('https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/accounts/update', newData);
+            axios.post('https://cors-anywhere.herokuapp.com/https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/accounts/update', newData, config)
+            .then(res =>{
+                console.log(res);
+                window.location.reload(false);
+                /*history.push('/'); */
+            },
+            err =>{
+                if(err.response){
+                    console.log("error-response");
+                } else if(err.request){
+                    console.log("error-request");
+
+                }else{
+                    console.log("error-else");
+
+                }
+
+            })
+
         }
     }
 
         return (
             <div className="transaction-inner">
-                    <input type="disabled"  className="form-control"/>
+                    
                     <form onSubmit ={e => {e.preventDefault(); handleTransaction(e);}}> 
+                    <div className="form-group" >
+                        <label>Avaliable Balance</label>
+                        <input type="disabled" disabled className="form-control" value={bal}/>
+                    </div>
+                    
                     <div className="form-group" >
                         <label>Transfer to</label>
                         <input type="text" className="form-control" placeholder="Mobile Numer / UEN" />
