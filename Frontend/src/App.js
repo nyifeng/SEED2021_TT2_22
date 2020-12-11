67a0321d8ecac09d619b9ddaeb5e57218788718a
Faqih <e0052804@u.nus.edu>
2020-12-11 13:54:05 +0800

    Use routers to route to pages

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import Home from './Views/Home'
+import Login from './Views/Login';
 
 function App() {
   return (
-    <div className="relative pb-10 min-h-screen">
-        <Header />
-        <Home />
-        <Footer />
-    </div>
+    <Router>
+        <div className="relative pb-10 min-h-screen">
+            <Switch>
+                <Route path='/home'>
+                    <Header />
+                    <Home />
+                    <Footer />
+                </Route>
+                <Route path='/login'>
+                    <Header />
+                    <Login />
+                    <Footer />
+                </Route>
+            </Switch>
+        </div>
+    </Router>
   );
 }
 
