200ca3710e9a56c623cdcc1a158d0f6ebd438a92
Faqih <e0052804@u.nus.edu>
2020-12-11 13:59:27 +0800

    Move Header and Footer out of Switch

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -9,18 +9,16 @@ function App() {
   return (
     <Router>
         <div className="relative pb-10 min-h-screen">
-            <Switch>
-                <Route path='/home'>
-                    <Header />
-                    <Home />
-                    <Footer />
-                </Route>
-                <Route path='/login'>
-                    <Header />
-                    <Login />
-                    <Footer />
-                </Route>
-            </Switch>
+            <Header />
+                <Switch>
+                    <Route path='/home'>
+                        <Home />
+                    </Route>
+                    <Route path='/login'>
+                        <Login />
+                    </Route>
+                </Switch>
+            <Footer />
         </div>
     </Router>
   );
