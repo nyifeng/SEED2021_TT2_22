b29221d5eb95ab0afb762d6b418f5f071038a82e
nicolechai2017 <nicole.chai.2017@sis.smu.edu.sg>
2020-12-11 14:44:28 +0800

    Added Dashboard to App

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Components/Header'
 import Footer from './Components/Footer'
+<<<<<<< Updated upstream
 import Dashboard from './Views/Dashboard'
 
 function App() {
@@ -10,6 +11,32 @@ function App() {
         <Dashboard />
         <Footer />
     </div>
+=======
+import Home from './Views/Home'
+import Login from './Views/Login';
+import Dashboard from './Views/Dashboard';
+
+
+function App() {
+  return (
+    <Router>
+        <div className="relative pb-10 min-h-screen">
+            <Header />
+                <Switch>
+                    <Route path='/home'>
+                        <Home />
+                    </Route>
+                    <Route path='/login'>
+                        <Login />
+                    </Route>
+                    <Route path='/dashboard'>
+                        <Dashboard />
+                    </Route>
+                </Switch>
+            <Footer />
+        </div>
+    </Router>
+>>>>>>> Stashed changes
   );
 }
 
